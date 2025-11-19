4cbe14a628591b9995a143029935c998ae8715c1
Stepan Ryzhkov <clouztoyou@mail.ru>
2025-11-19 14:41:56 +0300

    pong hardcode

--- app/main.go
+++ app/main.go
@@ -20,9 +20,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	// buff := make([]byte, 1024)
+	// n, err := conn.Read(buff)
+	// if n == 0 {
+	// 	fmt.Println("No command")
+	// }
+
+	//if string(buff) == "+PING\r\n" {
+	conn.Write([]byte("+PONG\r\n"))
+	//}
 }
